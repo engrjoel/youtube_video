7cb307a747cb8cf4c054fcc2c3c17eb66b02cfa1
Yuriy Afanasiev <33378584+yuri-afanasiev@users.noreply.github.com>
2019-02-14 12:23:43 +0200

    Add files via upload

---
+++ code/4_Arduino _ESP8266_MQTT/arduino_ReadAnalog/arduino_ReadAnalog.ino
@@ -0,0 +1,13 @@
+void setup() {
+  Serial.begin(9600);
+}
+void loop() { 
+     String test = "Aa"; 
+            test += analogRead(A0);
+            test += "aA";  
+     Serial.println(test);//Aa100aA
+     delay(1000); 
+}
+
+ 
+ 
